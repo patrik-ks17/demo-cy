fba066c3256c69c5518c6cd27e6ecf3f654f5ebb
patrik-ks17 <kasaip02@gmail.com>
2023-11-23 15:48:12 +0100

    test db

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -1,5 +1,14 @@
 describe('Dashboard', () => {
     
+    // before(() => {
+    //     cy.exec('cp .env .env.backup');
+    //     cy.exec('cp .env.cypress .env');
+    // })
+
+    // after(() => {
+    //     cy.exec('mv .env.backup .env');
+    // })
+
     it('should not allow guests to view the dashboard', () => {
         cy.visit('/home')
             .url().should('include', '/login')
@@ -10,5 +19,6 @@ describe('Dashboard', () => {
         // and they visit the login page
         // and type in their credentials
         // they should then be redirected to the dashboard
+
     })
 })
